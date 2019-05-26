0d90f93340f26f26478d1e783980df9cc088b76c
beccabear21 <beccabearcox@gmail.com>
2019-05-25 18:45:48 -0700

    first change

--- survey.js
+++ survey.js
@@ -4,7 +4,3 @@ here i am
 echo "# survey-form" >> README.md
 git init
 git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/beccabear21/survey-form.git
-git push -u origin master
-gi
